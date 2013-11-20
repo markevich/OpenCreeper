42a636a9f0c732ec0110517702cfdf1cbdb91a21
Alexander Zeillinger <alexanderzeillinger@gmail.com>
2013-11-20 19:38:19 +0100

    Add layers to Renderer to avoid constant sorting

--- dart/renderer.dart
+++ dart/renderer.dart
@@ -4,12 +4,16 @@ class Renderer {
   CanvasElement view;
   CanvasRenderingContext2D context;
   int top, left, bottom, right;
-  List<DisplayObject> displayObjects = new List<DisplayObject>();
+  List<List<DisplayObject>> layers = new List<List<DisplayObject>>(20);
 
   Renderer(this.view, width, height) {
     updateRect(width, height);
     view.style.position = "absolute";
     context = view.getContext('2d');
+    
+    for (int i = 0; i < layers.length; i++) {
+      layers[i] = new List<DisplayObject>();
+    }
   }
 
   void clear() {
@@ -26,18 +30,11 @@ class Renderer {
   }
 
   void addDisplayObject(DisplayObject displayObject) {
-    displayObjects.add(displayObject);
-    sortDisplayObjects();
+    layers[displayObject.layer._value].add(displayObject);
   }
   
-  void sortDisplayObjects() {
-    displayObjects.sort((DisplayObject a, DisplayObject b) {
-      return a.layer - b.layer;
-    });
-  }
-
   void removeDisplayObject(DisplayObject displayObject) {
-    displayObjects.removeAt(displayObjects.indexOf(displayObject));
+    layers[displayObject.layer._value].removeAt(layers[displayObject.layer._value].indexOf(displayObject));
   }
   
   /**
@@ -55,108 +52,110 @@ class Renderer {
 
   void draw() {   
     
-    for (var displayObject in displayObjects) {
-      if (displayObject.visible) {
-
-        // render sprite
-        if (displayObject is Sprite) {
-          Vector realPosition = displayObject.position.real2screen();
-
-          if (isVisible(realPosition, displayObject.size)) {
-
-            if (displayObject.alpha != 1.0)
-              context.globalAlpha = displayObject.alpha;
-
-            if (displayObject.rotation != 0) {
-              context.save();
-              context.translate(realPosition.x, realPosition.y);
-              context.rotate(engine.deg2rad(displayObject.rotation));
-              if (displayObject.animated)
-                context.drawImageScaledFromSource(displayObject.image,
-                (displayObject.frame % 8) * displayObject.size.x,
-                (displayObject.frame ~/ 8) * displayObject.size.y,
-                displayObject.size.x,
-                displayObject.size.y,
-                -displayObject.size.x * displayObject.anchor.x * displayObject.scale.x * game.zoom,
-                -displayObject.size.y * displayObject.anchor.y * displayObject.scale.y * game.zoom,
-                displayObject.size.x * displayObject.scale.x * game.zoom,
-                displayObject.size.y * displayObject.scale.y * game.zoom);
-              else
-                context.drawImageScaled(displayObject.image,
-                -displayObject.size.x * displayObject.anchor.x * displayObject.scale.x * game.zoom,
-                -displayObject.size.y * displayObject.anchor.y * displayObject.scale.y * game.zoom,
-                displayObject.size.x * displayObject.scale.x * game.zoom,
-                displayObject.size.y * displayObject.scale.y * game.zoom);
-              context.restore();
-            } else {
-              if (displayObject.animated)
-                context.drawImageScaledFromSource(displayObject.image,
-                (displayObject.frame % 8) * displayObject.size.x,
-                (displayObject.frame ~/ 8) * displayObject.size.y,
-                displayObject.size.x,
-                displayObject.size.y,
-                realPosition.x - displayObject.size.x * displayObject.anchor.x * displayObject.scale.x * game.zoom,
-                realPosition.y - displayObject.size.y * displayObject.anchor.y * displayObject.scale.y * game.zoom,
-                displayObject.size.x * displayObject.scale.x * game.zoom,
-                displayObject.size.y * displayObject.scale.y * game.zoom);
-              else
-                context.drawImageScaled(displayObject.image,
-                realPosition.x - displayObject.size.x * displayObject.anchor.x * displayObject.scale.x * game.zoom,
-                realPosition.y - displayObject.size.y * displayObject.anchor.y * displayObject.scale.y * game.zoom,
-                displayObject.size.x * displayObject.scale.x * game.zoom,
-                displayObject.size.y * displayObject.scale.y * game.zoom);
-            }
-
-            if (displayObject.alpha != 1.0)
-              context.globalAlpha = 1.0;
-          }
-        }
-
-        // render rectangle
-        else if (displayObject is Rect) {
-          Vector realPosition = displayObject.position.real2screen();
-
-          if (isVisible(realPosition, displayObject.size * game.zoom)) {
-            context.lineWidth = displayObject.lineWidth;
-            context.fillStyle = displayObject.color;
-            context.fillRect(realPosition.x - displayObject.size.x * displayObject.anchor.x * game.zoom,
-                             realPosition.y - displayObject.size.y * displayObject.anchor.y * game.zoom,
-                             displayObject.size.x * game.zoom,
-                             displayObject.size.y * game.zoom);
-          }
-        }
-
-        // render circle
-        else if (displayObject is Circle) {
-          Vector realPosition = displayObject.position.real2screen();
-
-          if (isVisible(realPosition, new Vector(displayObject.radius * displayObject.scale * game.zoom, displayObject.radius * displayObject.scale * game.zoom))) {
-            context.lineWidth = displayObject.lineWidth * game.zoom;
-            context.strokeStyle = displayObject.color;
-            context.beginPath();
-            context.arc(realPosition.x, realPosition.y, displayObject.radius * displayObject.scale * game.zoom, 0, PI * 2, true);
-            context.closePath();
-            context.stroke();
-          }
-        }
-
-        // render line
-        else if (displayObject is Line) {
-          Rectangle myview = new Rectangle(0, 0, view.width, view.height);   
-          
-          Vector realPositionFrom = displayObject.from.real2screen();
-          Vector realPositionTo = displayObject.to.real2screen();
-          
-          // check if line is visible
-          if (myview.containsPoint(new Point(realPositionFrom.x, realPositionFrom.y)) ||
-              myview.containsPoint(new Point(realPositionTo.x, realPositionTo.y))) {
-            context.lineWidth = displayObject.lineWidth;
-            context.strokeStyle = displayObject.color;
+    for (var layer in layers) {
+      for (var displayObject in layer) {
+        if (displayObject.visible) {
   
-            context.beginPath();
-            context.moveTo(realPositionFrom.x, realPositionFrom.y);
-            context.lineTo(realPositionTo.x, realPositionTo.y);
-            context.stroke();
+          // render sprite
+          if (displayObject is Sprite) {
+            Vector realPosition = displayObject.position.real2screen();
+  
+            if (isVisible(realPosition, displayObject.size)) {
+  
+              if (displayObject.alpha != 1.0)
+                context.globalAlpha = displayObject.alpha;
+  
+              if (displayObject.rotation != 0) {
+                context.save();
+                context.translate(realPosition.x, realPosition.y);
+                context.rotate(engine.deg2rad(displayObject.rotation));
+                if (displayObject.animated)
+                  context.drawImageScaledFromSource(displayObject.image,
+                  (displayObject.frame % 8) * displayObject.size.x,
+                  (displayObject.frame ~/ 8) * displayObject.size.y,
+                  displayObject.size.x,
+                  displayObject.size.y,
+                  -displayObject.size.x * displayObject.anchor.x * displayObject.scale.x * game.zoom,
+                  -displayObject.size.y * displayObject.anchor.y * displayObject.scale.y * game.zoom,
+                  displayObject.size.x * displayObject.scale.x * game.zoom,
+                  displayObject.size.y * displayObject.scale.y * game.zoom);
+                else
+                  context.drawImageScaled(displayObject.image,
+                  -displayObject.size.x * displayObject.anchor.x * displayObject.scale.x * game.zoom,
+                  -displayObject.size.y * displayObject.anchor.y * displayObject.scale.y * game.zoom,
+                  displayObject.size.x * displayObject.scale.x * game.zoom,
+                  displayObject.size.y * displayObject.scale.y * game.zoom);
+                context.restore();
+              } else {
+                if (displayObject.animated)
+                  context.drawImageScaledFromSource(displayObject.image,
+                  (displayObject.frame % 8) * displayObject.size.x,
+                  (displayObject.frame ~/ 8) * displayObject.size.y,
+                  displayObject.size.x,
+                  displayObject.size.y,
+                  realPosition.x - displayObject.size.x * displayObject.anchor.x * displayObject.scale.x * game.zoom,
+                  realPosition.y - displayObject.size.y * displayObject.anchor.y * displayObject.scale.y * game.zoom,
+                  displayObject.size.x * displayObject.scale.x * game.zoom,
+                  displayObject.size.y * displayObject.scale.y * game.zoom);
+                else
+                  context.drawImageScaled(displayObject.image,
+                  realPosition.x - displayObject.size.x * displayObject.anchor.x * displayObject.scale.x * game.zoom,
+                  realPosition.y - displayObject.size.y * displayObject.anchor.y * displayObject.scale.y * game.zoom,
+                  displayObject.size.x * displayObject.scale.x * game.zoom,
+                  displayObject.size.y * displayObject.scale.y * game.zoom);
+              }
+  
+              if (displayObject.alpha != 1.0)
+                context.globalAlpha = 1.0;
+            }
+          }
+  
+          // render rectangle
+          else if (displayObject is Rect) {
+            Vector realPosition = displayObject.position.real2screen();
+  
+            if (isVisible(realPosition, displayObject.size * game.zoom)) {
+              context.lineWidth = displayObject.lineWidth;
+              context.fillStyle = displayObject.color;
+              context.fillRect(realPosition.x - displayObject.size.x * displayObject.anchor.x * game.zoom,
+                               realPosition.y - displayObject.size.y * displayObject.anchor.y * game.zoom,
+                               displayObject.size.x * game.zoom,
+                               displayObject.size.y * game.zoom);
+            }
+          }
+  
+          // render circle
+          else if (displayObject is Circle) {
+            Vector realPosition = displayObject.position.real2screen();
+  
+            if (isVisible(realPosition, new Vector(displayObject.radius * displayObject.scale * game.zoom, displayObject.radius * displayObject.scale * game.zoom))) {
+              context.lineWidth = displayObject.lineWidth * game.zoom;
+              context.strokeStyle = displayObject.color;
+              context.beginPath();
+              context.arc(realPosition.x, realPosition.y, displayObject.radius * displayObject.scale * game.zoom, 0, PI * 2, true);
+              context.closePath();
+              context.stroke();
+            }
+          }
+  
+          // render line
+          else if (displayObject is Line) {
+            Rectangle myview = new Rectangle(0, 0, view.width, view.height);   
+            
+            Vector realPositionFrom = displayObject.from.real2screen();
+            Vector realPositionTo = displayObject.to.real2screen();
+            
+            // check if line is visible
+            if (myview.containsPoint(new Point(realPositionFrom.x, realPositionFrom.y)) ||
+                myview.containsPoint(new Point(realPositionTo.x, realPositionTo.y))) {
+              context.lineWidth = displayObject.lineWidth;
+              context.strokeStyle = displayObject.color;
+    
+              context.beginPath();
+              context.moveTo(realPositionFrom.x, realPositionFrom.y);
+              context.lineTo(realPositionTo.x, realPositionTo.y);
+              context.stroke();
+            }
           }
         }
       }
